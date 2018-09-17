b2696cfa7da138e62aed644543501e56bf7c7771
nathanwong Wong <nathanwong@berkeley.edu>
2018-09-17 15:38:58 -0700

    updated sh file with sequential output

--- mkrandom.sh
+++ mkrandom.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 for run in {1..$1}
 do
-  dd if=/dev/urandom iflag=fullblock of=file bs=1 count=$2
+  i = 01
+  dd if=/dev/urandom iflag=fullblock of="FILE$i" bs=1 count=$2
+  do
+    i=$(printf "%02d" $((i+1)))
 done
