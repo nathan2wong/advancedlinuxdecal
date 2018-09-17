3e1d1f30d660b11ef8899c73081ee18abd4ef383
nathanwong Wong <nathanwong@berkeley.edu>
2018-09-17 15:40:28 -0700

    run variable

--- mkrandom.sh
+++ mkrandom.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 for run in {1..$1}
 do
-  i = 01
-  dd if=/dev/urandom iflag=fullblock of="FILE$i" bs=1 count=$2
-  do
-    i=$(printf "%02d" $((i+1)))
-  done
+  dd if=/dev/urandom iflag=fullblock of="FILE$run" bs=1 count=$2
 done
